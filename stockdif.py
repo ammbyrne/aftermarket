cd87c499c21c4703626d8e46ce32c5da8a9909cf
ammbyrne <andy_m_byrne@yahoo.co.uk>
2022-08-01 21:38:14 +0800

    new

--- stockdif.py
+++ stockdif.py
@@ -36,13 +36,14 @@ def runDif():
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    #chrome_options.add_argument("--headless")
-    #chrome_options.add_argument("--disable-dev-shm-usage")
-    #chrome_options.add_argument("--no-sandbox")
+
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
     #driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-    ser = Service(os.environ.get("CHROMEDRIVER_PATH"))
+    ser = Service("/app/.chromedriver/bin/chromedriver")
    # driver = webdriver.Chrome(service=ser, options=chrome_options)
-    driver = webdriver.Chrome(service=ser)
+    driver = webdriver.Chrome(service=ser, options=chrome_options)
 
     driver.get("https://bullsheet.me/auth/login")
     #driver.fullscreen_window()
